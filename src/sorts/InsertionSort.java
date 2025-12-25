21fdcd6f5af36f28db7a02aecd4ebc6e2abb15e8
Praveen Thathsara <praveenstudy823@gmail.com>
2025-12-25 15:30:40 +0530

    operation1

---
+++ src/sorts/InsertionSort.java
@@ -0,0 +1,25 @@
+package sorts;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+
+        int[] arr = {3,5,2,4,1};
+        int n = arr.length;
+
+        for(int i=1; i<n; i++){
+            int key = arr[i];
+            int j = i-1;
+
+            while(j >= 0 && arr[j] > key){
+                arr[j+1] = arr[j];
+                j--;
+            }
+
+            arr[j+1] = key;
+        }
+
+        for (int a : arr){
+            System.out.print(a+" ");
+        }
+    }
+}
